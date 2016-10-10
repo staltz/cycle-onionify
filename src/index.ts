df30ee5b1d5b49d961e999c53344956a8c2a04c9
Andre Staltz <andre@staltz.com>
2016-10-10 21:39:23 +0300

    Use Object.assign instead of custom implementation

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
 import xs, {Stream, MemoryStream} from 'xstream';
 
-const hasOwnProperty = Object.prototype.hasOwnProperty;
-function objectExtend<T>(oldObj: T, newObj: T): T {
-  var target = {};
-  for (var key in oldObj) {
-    if (hasOwnProperty.call(oldObj, key)) {
-      target[key] = oldObj[key];
-    }
-  }
-  for (var key in newObj) {
-    if (hasOwnProperty.call(newObj, key)) {
-      target[key] = newObj[key];
-    }
-  }
-  return <T> target;
-}
-
 export type MainFn<Sources, Sinks> = (sources: Sources) => Sinks;
 export type Reducer = (state: any) => any;
 export type Selector = (state: any) => any;
@@ -73,7 +57,7 @@ export class StateSource<T> {
       if (Array.isArray(state) && typeof index === 'number') {
         return updateArrayEntry(state, index, reducer);
       } else {
-        return objectExtend(state, {[scope]: reducer(state[scope])});
+        return Object.assign({}, state, {[scope]: reducer(state[scope])});
       }
     });
   }
