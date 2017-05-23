8168660583ec7f9d8b3e3efc465928c3edc596b3
Andre Staltz <andre@staltz.com>
2017-05-23 13:06:27 +0300

    Fix typings so onionify returns any=>any, due to missing TypeScript features

--- src/index.ts
+++ src/index.ts
@@ -189,18 +189,47 @@ export class StateSource<T> {
   public isolateSink = isolateSink;
 }
 
-export default function onionify<So, Si>(
+/**
+ * While we are waiting for keyof subtraction to land in TypeScript,
+ * https://github.com/Microsoft/TypeScript/issues/12215,
+ * we must use `any` as the type of sources or sinks in the mainOnionified.
+ * This is because the correct type is *not*
+ *
+ * Main<So, Si>
+ *
+ * *neither*
+ *
+ * Main<Partial<So>, Partial<Si>>
+ *
+ * The former will signal to Cycle.run that a driver for 'onion' is needed,
+ * while the latter will make valid channels like 'DOM' become optional.
+ * The correct type should be
+ *
+ * Main<Omit<So, 'onion'>, Omit<Si, 'onion'>>
+ */
+export type Omit<T, K extends keyof T> = any;
+// type Omit<T, K extends keyof T> = {
+//     [P in keyof T - K]: T[P];
+// };
+
+export type OSo<T> = {onion: StateSource<T>};
+export type OSi<T> = {onion: Stream<Reducer<T>>};
+
+export type MainOnionified<T, So extends OSo<T>, Si extends OSi<T>> =
+  MainFn<Omit<So, 'onion'>, Omit<Si, 'onion'>>;
+
+export default function onionify<T, So extends OSo<T>, Si extends OSi<T>>(
                                 main: MainFn<So, Si>,
-                                name: string = 'onion'): MainFn<So, Si> {
+                                name: string = 'onion'): MainOnionified<T, So, Si> {
   return function mainOnionified(sources: So): Si {
-    const reducerMimic$ = xs.create<Reducer<any>>();
+    const reducerMimic$ = xs.create<Reducer<T>>();
     const state$ = reducerMimic$
-      .fold((state, reducer) => reducer(state), void 0)
+      .fold((state, reducer) => reducer(state), void 0 as (T | undefined))
       .drop(1);
     sources[name as any] = new StateSource<any>(state$, name) as any;
     const sinks = main(sources as So);
     if (sinks[name]) {
-      const stream$ = xs.fromObservable<Reducer<any>>(sinks[name]);
+      const stream$ = xs.fromObservable<Reducer<T>>(sinks[name]);
       reducerMimic$.imitate(stream$);
     }
     return sinks;
