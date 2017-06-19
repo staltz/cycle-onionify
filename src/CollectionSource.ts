efc5ed8620bb9238768d55bc99b570c13b1785a8
Andre Staltz <andre@staltz.com>
2017-06-19 12:51:11 +0300

    Cosmetic refactor: move collection-related code to CollectionSource

--- src/CollectionSource.ts
+++ src/CollectionSource.ts
@@ -1,11 +1,93 @@
 import {Stream} from 'xstream';
 import {adapt} from '@cycle/run/lib/adapt';
+import isolate from '@cycle/isolate';
 import {pickMerge} from './pickMerge';
 import {pickCombine} from './pickCombine';
-import {Instances} from './types';
+import {Instances, Lens} from './types';
+
+const identityLens = {
+  get: <T>(outer: T) => outer,
+  set: <T>(outer: T, inner: T) => inner,
+};
+
+function instanceLens(getKey: any, key: string): Lens<Array<any>, any> {
+  return {
+    get(arr: Array<any> | undefined): any {
+      if (typeof arr === 'undefined') {
+        return void 0;
+      } else {
+        for (let i = 0, n = arr.length; i < n; ++i) {
+          if (getKey(arr[i]) === key) {
+            return arr[i];
+          }
+        }
+        return void 0;
+      }
+    },
+
+    set(arr: Array<any> | undefined, item: any): any {
+      if (typeof arr === 'undefined') {
+        return [item];
+      } else if (typeof item === 'undefined') {
+        return arr.filter(s => getKey(s) !== key);
+      } else {
+        return arr.map(s => {
+          if (getKey(s) === key) {
+            return item;
+          } else {
+            return s;
+          }
+        });
+      }
+    },
+  };
+}
 
 export class CollectionSource<Si> {
-  constructor(private _ins$: Stream<Instances<Si>>) { }
+  private _instances$: Stream<Instances<Si>>;
+
+  constructor(itemComp: (so: any) => Si,
+              sources: any,
+              state$: Stream<Array<any> | any>,
+              name: string,
+              getKey: any) {
+    this._instances$ = state$.fold((acc: Instances<Si>, nextState: Array<any> | any) => {
+      const dict = acc.dict;
+      if (Array.isArray(nextState)) {
+        const nextInstArray = Array(nextState.length) as Array<Si & {_key: string}>;
+        const nextKeys = new Set<string>();
+        // add
+        for (let i = 0, n = nextState.length; i < n; ++i) {
+          const key = getKey(nextState[i]);
+          nextKeys.add(key);
+          if (dict.has(key)) {
+            nextInstArray[i] = dict.get(key) as any;
+          } else {
+            const scopes = {'*': '$' + key, [name]: instanceLens(getKey, key)};
+            const sinks = isolate(itemComp, scopes)(sources);
+            dict.set(key, sinks);
+            nextInstArray[i] = sinks;
+          }
+          nextInstArray[i]._key = key;
+        }
+        // remove
+        dict.forEach((_, key) => {
+          if (!nextKeys.has(key)) {
+            dict.delete(key);
+          }
+        });
+        nextKeys.clear();
+        return {dict: dict, arr: nextInstArray};
+      } else {
+        dict.clear();
+        const key = getKey(nextState);
+        const scopes = {'*': '$' + key, [name]: identityLens};
+        const sinks = isolate(itemComp, scopes)(sources);
+        dict.set(key, sinks);
+        return {dict: dict, arr: [sinks]}
+      }
+    }, {dict: new Map(), arr: []} as Instances<Si>);
+  }
 
   /**
    * Like `merge` in xstream, this operator blends multiple streams together, but
@@ -19,7 +101,7 @@ export class CollectionSource<Si> {
    * @return {Function} an operator to be used with xstream's `compose` method.
    */
   public pickMerge(selector: string): Stream<any> {
-    return adapt(this._ins$.compose(pickMerge(selector)));
+    return adapt(this._instances$.compose(pickMerge(selector)));
   }
 
   /**
@@ -34,6 +116,6 @@ export class CollectionSource<Si> {
    * @return {Function} an operator to be used with xstream's `compose` method.
    */
   public pickCombine(selector: string): Stream<Array<any>> {
-    return adapt(this._ins$.compose(pickCombine(selector)));
+    return adapt(this._instances$.compose(pickCombine(selector)));
   }
 }
--- src/StateSource.ts
+++ src/StateSource.ts
@@ -2,7 +2,6 @@ import xs, {Stream, MemoryStream} from 'xstream';
 import dropRepeats from 'xstream/extra/dropRepeats';
 import {DevToolEnabledSource} from '@cycle/run';
 import {adapt} from '@cycle/run/lib/adapt';
-import isolate from '@cycle/isolate';
 import {CollectionSource} from './CollectionSource';
 import {Getter, Setter, Lens, Scope, Reducer, Instances} from './types';
 
@@ -75,44 +74,6 @@ function defaultGetKey(statePiece: any) {
   return statePiece.key;
 }
 
-const identityLens = {
-  get: <T>(outer: T) => outer,
-  set: <T>(outer: T, inner: T) => inner,
-};
-
-function instanceLens(getKey: any, key: string): Lens<Array<any>, any> {
-  return {
-    get(arr: Array<any> | undefined): any {
-      if (typeof arr === 'undefined') {
-        return void 0;
-      } else {
-        for (let i = 0, n = arr.length; i < n; ++i) {
-          if (getKey(arr[i]) === key) {
-            return arr[i];
-          }
-        }
-        return void 0;
-      }
-    },
-
-    set(arr: Array<any> | undefined, item: any): any {
-      if (typeof arr === 'undefined') {
-        return [item];
-      } else if (typeof item === 'undefined') {
-        return arr.filter(s => getKey(s) !== key);
-      } else {
-        return arr.map(s => {
-          if (getKey(s) === key) {
-            return item;
-          } else {
-            return s;
-          }
-        });
-      }
-    },
-  };
-}
-
 export class StateSource<T> {
   public state$: MemoryStream<T>;
   private _state$: MemoryStream<T>;
@@ -176,47 +137,7 @@ export class StateSource<T> {
   public asCollection<Si>(itemComp: (so: any) => Si,
                           sources: any,
                           getKey: any = defaultGetKey): CollectionSource<Si> {
-    const array$ = this._state$;
-    const name = this._name;
-
-    const collection$ = array$.fold((acc: Instances<Si>, nextState: Array<any> | any) => {
-      const dict = acc.dict;
-      if (Array.isArray(nextState)) {
-        const nextInstArray = Array(nextState.length) as Array<Si & {_key: string}>;
-        const nextKeys = new Set<string>();
-        // add
-        for (let i = 0, n = nextState.length; i < n; ++i) {
-          const key = getKey(nextState[i]);
-          nextKeys.add(key);
-          if (dict.has(key)) {
-            nextInstArray[i] = dict.get(key) as any;
-          } else {
-            const scopes = {'*': '$' + key, [name]: instanceLens(getKey, key)};
-            const sinks = isolate(itemComp, scopes)(sources);
-            dict.set(key, sinks);
-            nextInstArray[i] = sinks;
-          }
-          nextInstArray[i]._key = key;
-        }
-        // remove
-        dict.forEach((_, key) => {
-          if (!nextKeys.has(key)) {
-            dict.delete(key);
-          }
-        });
-        nextKeys.clear();
-        return {dict: dict, arr: nextInstArray};
-      } else {
-        dict.clear();
-        const key = getKey(nextState);
-        const scopes = {'*': '$' + key, [name]: identityLens};
-        const sinks = isolate(itemComp, scopes)(sources);
-        dict.set(key, sinks);
-        return {dict: dict, arr: [sinks]}
-      }
-    }, {dict: new Map(), arr: []} as Instances<Si>);
-
-    return new CollectionSource<Si>(collection$);
+    return new CollectionSource<Si>(itemComp, sources, this._state$, this._name, getKey);
   }
 
   public isolateSource = isolateSource;
